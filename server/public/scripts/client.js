64f9806882db4c7dd6661c3334171dff3c47c50a
Torben Jepsen <torbenjepsen@outlook.com>
2018-04-19 12:01:26 -0500

    styling

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,13 +5,23 @@ $(document).ready(onReady);
 function onReady() {
     console.log('jQuery has been loaded');
     $.ajax({
-        type: 'GET',
+        method: 'GET',
         url: '/records'
     })
+        // method or type can be used in jquery. not in angular
         .then(function (response) {
             console.log(response);
-            for (let i = 0; i < response.length; i++) {
-                $('#records').append('<ul><li>' + response[i].title + ' - ' + response[i].year + ' : ' + response[i].artist + ' $' + response[i].cost + '</li></ul>');
-            }
+            // for (let i = 0; i < response.length; i++) {
+            //     $('#records').append('<ul><li>' + response[i].title + ' - ' + response[i].year + ' : ' + response[i].artist + ' $' + response[i].cost + '</li></ul>');
+            // }
+            // a forEach loop could be used for less code:
+            response.forEach(function (record) {
+                $('#records').append(`<tr>
+                    <td>${record.title}</td>
+                    <td>${record.artist}</td>
+                    <td>${record.year}</td>
+                    <td>${record.cost.toLocaleString('en', { style: 'currency', currency: 'USD'}).slice(0, -3)}</td>
+                    </tr>`);
+            });
         });
 }
