78b158011a4e50ce5bf7adeda4f88da4247ba262
Torben Jepsen <torbenjepsen@outlook.com>
2018-04-19 11:28:57 -0500

    appended array to DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,14 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('jQuery has been loaded');
+    $.ajax({
+        type: 'GET',
+        url: '/records'
+    })
+        .then(function (response) {
+            console.log(response);
+            for (let i = 0; i < response.length; i++) {
+                $('#records').append('<ul><li>' + response[i].title + ' - ' + response[i].year + ' : ' + response[i].artist + ' $' + response[i].cost + '</li></ul>');
+            }
+        });
 }
--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
 const PORT = 5000;
+// these always need to be at the top
+
+const recordCollection = require('./modules/record-collection.js')
+// this will live in the database shortlw=y
 
 app.use(express.static('server/public'));
+// always need static here
 
+// routes (get, post, put delete requests)
+
+app.get('/records', (req, res) => {
+    res.send(recordCollection);
+});
 
 app.listen(PORT, () => {
     console.log(`Up and Running on port ${PORT}`);
