9335d855e0133dd2f88c0a60bd2748829409234c
Torben Jepsen <torbenjepsen@outlook.com>
2018-04-20 14:29:19 -0500

    added post

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,6 +4,11 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('jQuery has been loaded');
+    $('#addNewSong').on('click', addNew);
+    getAllSongs();
+}
+
+function getAllSongs() {
     $.ajax({
         method: 'GET',
         url: '/records'
@@ -11,17 +16,38 @@ function onReady() {
         // method or type can be used in jquery. not in angular
         .then(function (response) {
             console.log(response);
+            $('#records').empty();
             // for (let i = 0; i < response.length; i++) {
             //     $('#records').append('<ul><li>' + response[i].title + ' - ' + response[i].year + ' : ' + response[i].artist + ' $' + response[i].cost + '</li></ul>');
             // }
-            // a forEach loop could be used for less code:
+            // a forEach loop could be used for less code (for each loops can only be used for arrays!):
             response.forEach(function (record) {
                 $('#records').append(`<tr>
                     <td>${record.title}</td>
                     <td>${record.artist}</td>
                     <td>${record.year}</td>
-                    <td>${record.cost.toLocaleString('en', { style: 'currency', currency: 'USD'}).slice(0, -3)}</td>
+                    <td>${record.cost.toLocaleString('en', { style: 'currency', currency: 'USD' }).slice(0, -3)}</td>
                     </tr>`);
             });
         });
+}
+
+function addNew(title, artist, year, cost) {
+    const newSong = {
+        title: $('#title').val(),
+        artist: $('#artist').val(),
+        year: $('#year').val(),
+        cost: $('#cost').val()
+    };
+    console.log('New song object', newSong);
+    $.ajax({
+        method: 'POST',
+        url: '/add-record',
+        data: newSong
+    })
+        .then(function (response) {
+            console.log(response);
+           getAllSongs();
+        });
+        
 }
--- server/server.js
+++ server/server.js
@@ -1,19 +1,30 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
 // these always need to be at the top
 
 const recordCollection = require('./modules/record-collection.js')
-// this will live in the database shortlw=y
+// this will live in the database shortly
 
 app.use(express.static('server/public'));
 // always need static here
+app.use(bodyParser.urlencoded({extended: true }));
+// need this line of code in order to post
 
 // routes (get, post, put delete requests)
 
 app.get('/records', (req, res) => {
     res.send(recordCollection);
 });
+
+app.post('/add-record', (req, res) => {
+    console.log(req.body);
+    recordCollection.push(req.body);
+    res.sendStatus(200);
+});
+
+// req.body on the server = data on our clientside aka newSong
 
 app.listen(PORT, () => {
     console.log(`Up and Running on port ${PORT}`);
